85440e5da63ea644bdd4e66b2603d6869ae638d8
jangjaeko <jangjaeko45@gmail.com>
2023-03-06 23:56:57 +0900

    test routing

--- src/pages/components/FirstSection/FirstSection.tsx
+++ src/pages/components/FirstSection/FirstSection.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function FirstSection() {
-  return <div>FirstSection</div>;
+  return <div>FirstSection to main</div>;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
+import FirstSection from "./components/FirstSection/FirstSection";
 import Header from "./components/Header/Header";
 
 export default function Home() {
   return (
     <Fragment>
       <Header />
-      <main></main>
+      <FirstSection />
     </Fragment>
   );
 }
