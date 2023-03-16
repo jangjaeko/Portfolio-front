b9114027bc4bbb36bb11cec69a582a262be7d27f
jangjaeko <jangjaeko45@gmail.com>
2023-03-16 17:27:15 +0900

    style : skills

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -10,12 +10,28 @@ import red from "./images/redux.png";
 import ty from "./images/typescript.png";
 import node from "./images/node.png";
 import nest from "./images/nest.svg";
+import rn from "./images/RN.png";
+import fb from "./images/Firebase.png";
+import gh from "./images/github.png";
+import sl from "./images/Slacklogo.png";
+import fi from "./images/figma.png";
+import ji from "./images/jira.png";
+import gi from "./images/git.png";
+import my from "./images/mysql.png";
+import php from "./images/php.png";
+import word from "./images/wordpress.png";
 export default function Skills() {
   return (
     <MainView>
       <Title>Skills</Title>
-      <ContentDiv className="row" style={{ width: "100%" }}>
-        <div className="col-md-6  col-lg-4">
+      <ContentDiv className="row">
+        <div
+          className="col-md-6  col-lg-4"
+          //   style={{
+          //     display: "flex",
+          //     justifyContent: "center",
+          //   }}
+        >
           <BigSection>
             <SkillsTitle>Frontend</SkillsTitle>
             <Hline />
@@ -25,7 +41,8 @@ export default function Skills() {
             <LogoImages src={red} alt="" />
             <LogoImages src={ne} alt="" />
             <LogoImages src={jq} alt="" />
-            <LogoImages src={boot} alt="" />
+            <SLogoImages src={boot} alt="" />
+            <BLogoImages src={word} alt="" />
           </BigSection>
         </div>
         <div className="col-md-6  col-lg-4">
@@ -33,17 +50,30 @@ export default function Skills() {
             <SkillsTitle>Backend</SkillsTitle>
             <Hline />
             <LogoImages src={node} alt="" />
-            <LogoImages src={nest} alt="" />
+            <SLogoImages src={nest} alt="" />
+            <SLogoImages src={fb} alt="" />
+            <LogoImages src={my} alt="" />
+            <LogoImages src={php} alt="" />
           </BigSection>
           <BigSection style={{ marginTop: "5vh" }}>
             <SkillsTitle>Mobile App</SkillsTitle>
             <Hline />
+            <SLogoImages src={rn} alt="" />
           </BigSection>
         </div>
         <div className="col-md-6  col-lg-4">
           <BigSection>
             <SkillsTitle>Version Control</SkillsTitle>
             <Hline />
+            <LogoImages src={gh} alt="" />
+            <SLogoImages src={gi} alt="" />
+          </BigSection>
+          <BigSection>
+            <SkillsTitle>Communication</SkillsTitle>
+            <Hline />
+            <BLogoImages src={sl} alt="" />
+            <BLogoImages src={fi} alt="" />
+            <BLogoImages src={ji} alt="" />
           </BigSection>
         </div>
       </ContentDiv>
@@ -61,16 +91,17 @@ const MainView = styled.div`
 const Title = styled.span`
   font-family: PreExBold;
   color: #1263ce;
-  font-size: 1.8rem;
+  font-size: 2.5rem;
 `;
 
 const BigSection = styled.div`
   ${({ theme }) => theme.common.flexColumn};
   background-color: #ffffff;
   border-radius: 15px;
-  width: 80%;
+  width: 90%;
   padding: 10%;
   margin-top: 5vh;
+  margin-left: 5%;
   box-shadow: 0.5px 0.5px 0.5px 0px;
 `;
 
@@ -79,10 +110,10 @@ const SkillsTitle = styled.span`
 `;
 
 const ContentDiv = styled.span`
-    margin-top: 2vh,
-    display: flex,
-    align-Items: center,
-    
+  display: flex;
+  width: 100%;
+  padding-left: 10%;
+  padding-right: 10%;
 `;
 const Hline = styled.div`
   width: 100%;
@@ -95,4 +126,15 @@ const LogoImages = styled(Image)`
   width: 100%;
   height: 13vh;
   margin-top: 3vh;
+`;
+
+const SLogoImages = styled(Image)`
+  width: 100%;
+  height: 10vh;
+  margin-top: 2vh;
+`;
+const BLogoImages = styled(Image)`
+  width: 100%;
+  height: 15vh;
+  margin-top: 1vh;
 `;
