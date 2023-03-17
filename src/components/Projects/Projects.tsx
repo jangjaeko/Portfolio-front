b3605635bf8373a851b4c59a9ff683dd84c5f60f
jangjaeko <jangjaeko45@gmail.com>
2023-03-17 23:32:41 +0900

    feat : project setting

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,6 +4,9 @@ export default function Projects() {
   return (
     <MainView>
       <Title>Projects</Title>
+      <BigSection>
+        <span>asdasdsa</span>
+      </BigSection>
     </MainView>
   );
 }
@@ -19,4 +22,13 @@ const Title = styled.span`
   font-family: PreExBold;
   color: #cddff5;
   font-size: 1.8rem;
+`;
+const BigSection = styled.div`
+  ${({ theme }) => theme.common.flexColumn};
+  background-color: #ffffff;
+  border-radius: 15px;
+  width: 75%;
+  padding: 10%;
+  margin-top: 5vh;
+  box-shadow: 0.5px 0.5px 0.5px 0px;
 `;
