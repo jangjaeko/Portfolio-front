b3fd8a7cecf143989d85bab1908636060586db2f
jangjaeko <jangjaeko45@gmail.com>
2023-03-20 23:26:11 +0900

    style : career

--- src/components/Career/Career.tsx
+++ src/components/Career/Career.tsx
@@ -4,6 +4,10 @@ export default function Career() {
   return (
     <MainView>
       <Title>Career</Title>
+      <Layer>
+        <Left></Left>
+        <Right></Right>
+      </Layer>
     </MainView>
   );
 }
@@ -13,10 +17,23 @@ const MainView = styled.div`
   background-color: #123123;
   padding-top: 10vh;
   padding-bottom: 10vh;
+  padding-left: 10%;
+  padding-right: 10%;
 `;
 
 const Title = styled.span`
   font-family: PreExBold;
   color: #cddff5;
   font-size: 1.8rem;
+`;
+
+const Layer = styled.div`
+  width: 100%;
+  ${({ theme }) => theme.common.flexRow};
+`;
+const Left = styled.div`
+  width: 30%;
+`;
+const Right = styled.div`
+  width: 30%;
 `;
