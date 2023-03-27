f1682506f6a71a2ec8044419273b4337e4c546b7
jangjaeko <jangjaeko45@gmail.com>
2023-03-27 18:08:28 +0900

    edit : icons

--- src/components/Career/InnerBlock.tsx
+++ src/components/Career/InnerBlock.tsx
@@ -2,35 +2,79 @@ import React from "react";
 import styled from "styled-components";
 import Image from "next/image";
 import Cla from "./images/Cla.png";
+import { BsFillAwardFill } from "react-icons/bs";
+import { BsFillCalendarCheckFill } from "react-icons/bs";
+import { BsFillPatchCheckFill } from "react-icons/bs";
+import { BsBookmarkFill } from "react-icons/bs";
+
 export default function InnerBlock() {
   return (
     <Layer>
-      <Left>
-        <Image src={Cla} style={{ width: "15vh", height: "10vh" }} alt="" />
-      </Left>
-      <Right>
-        <Title>클라코퍼레이션</Title>
-        <DateText>2021.11 ~ 2022.08</DateText>
-        <ComDesc>기술혁신을 통한 투명한 인플루언서 마케팅 스타트업</ComDesc>
-        <hr style={{ width: "100%" }} />
-        <DateText>주요 업무</DateText>
-        <Desc>
-          - React-native 와 Node JS 이용한 크로스 플랫폼 앱 개발
-          <br />- React 를 이용한 웹 프론트 개발 <br />
-          <br />
-        </Desc>
-        <hr style={{ width: "100%" }} />
-        <DateText>주요 프로젝트</DateText>
-        <hr style={{ width: "100%" }} />
-        <DateText>프로젝트 명 : MarkIn App</DateText>
-        <Work>사용언어 및 개발환경</Work>
-        <Desc>- React-native, JavaScript, HTML/CSS, TypeScript, Git</Desc>
-        <Work>주요업무 및 상세역할</Work>
-        <Desc>
-          - React-native 를 사용하여 크로스플랫폼 프론트앤드 개발
-          <br /> - NodeJS 를 통한 백앤드 개발 <br />- 출시 및 버전관리
-        </Desc>
-      </Right>
+      <div className="row">
+        <div className="col-md-4 col-lg-4 col-sm-12">
+          <Left>
+            <Image src={Cla} style={{ width: "15vh", height: "10vh" }} alt="" />
+          </Left>
+        </div>
+        <div className="col-sm-12">
+          <Right>
+            <Title> 클라코퍼레이션</Title>
+            <DateText>2021.11 ~ 2022.08</DateText>
+            <ComDesc>기술혁신을 통한 투명한 인플루언서 마케팅 스타트업</ComDesc>
+            <hr style={{ width: "100%" }} />
+            <DateText>
+              {" "}
+              <BsFillPatchCheckFill
+                size={25}
+                style={{ marginBottom: 5, marginRight: 5 }}
+              />{" "}
+              Major Work
+            </DateText>
+            <Desc>
+              - React-native 와 Node JS 이용한 크로스 플랫폼 앱 개발
+              <br />- React 를 이용한 웹 프론트 개발 <br />
+              <br />
+            </Desc>
+            <hr style={{ width: "100%" }} />
+            <DateText>
+              {" "}
+              <BsFillPatchCheckFill
+                size={25}
+                style={{ marginBottom: 5, marginRight: 5 }}
+              />
+              Major Project
+            </DateText>
+            <hr style={{ width: "100%" }} />
+            <DateText>
+              {" "}
+              <BsFillCalendarCheckFill
+                size={25}
+                style={{ marginBottom: 5, marginRight: 5 }}
+              />{" "}
+              Project Name : MarkIn App
+            </DateText>
+            <Work>
+              <BsBookmarkFill
+                size={20}
+                style={{ marginBottom: 5, marginRight: 5 }}
+              />
+              Language and development environment
+            </Work>
+            <Desc>- React-native, JavaScript, HTML/CSS, TypeScript, Git</Desc>
+            <Work>
+              <BsFillAwardFill
+                size={20}
+                style={{ marginBottom: 5, marginRight: 5 }}
+              />
+              Main tasks and details
+            </Work>
+            <Desc>
+              - React-native 를 사용하여 크로스플랫폼 프론트앤드 개발
+              <br /> - NodeJS 를 통한 백앤드 개발 <br />- 출시 및 버전관리
+            </Desc>
+          </Right>
+        </div>
+      </div>
     </Layer>
   );
 }
@@ -40,18 +84,14 @@ const Layer = styled.div`
   ${({ theme }) => theme.common.flexCenterRow};
   background-color: #ffffff;
   border-radius: 10px;
+  padding: 5%;
 `;
 const Left = styled.div`
-  width: 30%;
-  padding: 10%;
-  boder-style: solid;
-  border-right-color: #000000;
-  border-right-width: 15px;
+  width: 100%;
 `;
 const Right = styled.div`
   ${({ theme }) => theme.common.flexColumn};
-  width: 70%;
-  padding: 10%;
+  width: 100%;
   background-color: #ffffff;
 `;
 const Title = styled.span`
@@ -69,6 +109,7 @@ const ComDesc = styled.span`
 `;
 const Work = styled.span`
   font-family: PreMedium;
+  margin-top: 10px;
 `;
 const Desc = styled.span`
   font-family: PreLight;
